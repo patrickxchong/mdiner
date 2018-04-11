b0b8f1da5ac3553a743e8b462bcb2dd00bb71731
pcjh <pcjh@umich.edu>
2018-04-11 17:14:44 -0400

    home page!

--- app.py
+++ app.py
@@ -5,29 +5,29 @@ import os
 
 app = Flask(__name__, static_url_path="/static")
 
-# # to solve browser cache problem
-# @app.url_defaults
-# def hashed_url_for_static_file(endpoint, values):
-#     if 'static' == endpoint or endpoint.endswith('.static'):
-#         filename = values.get('filename')
-#         if filename:
-#             if '.' in endpoint:  # has higher priority
-#                 blueprint = endpoint.rsplit('.', 1)[0]
-#             else:
-#                 blueprint = request.blueprint  # can be None too
+# to solve browser cache problem
+@app.url_defaults
+def hashed_url_for_static_file(endpoint, values):
+    if 'static' == endpoint or endpoint.endswith('.static'):
+        filename = values.get('filename')
+        if filename:
+            if '.' in endpoint:  # has higher priority
+                blueprint = endpoint.rsplit('.', 1)[0]
+            else:
+                blueprint = request.blueprint  # can be None too
 
-#             if blueprint:
-#                 static_folder = app.blueprints[blueprint].static_folder
-#             else:
-#                 static_folder = app.static_folder
+            if blueprint:
+                static_folder = app.blueprints[blueprint].static_folder
+            else:
+                static_folder = app.static_folder
 
-#             param_name = 'h'
-#             while param_name in values:
-#                 param_name = '_' + param_name
-#             values[param_name] = static_file_hash(os.path.join(static_folder, filename))
+            param_name = 'h'
+            while param_name in values:
+                param_name = '_' + param_name
+            values[param_name] = static_file_hash(os.path.join(static_folder, filename))
             
-# def static_file_hash(filename):
-#   return int(os.stat(filename).st_mtime)
+def static_file_hash(filename):
+  return int(os.stat(filename).st_mtime)
 
 
 @app.route('/')
