f2c100f42800584c6208f53089d522532fd31779
pcjh <pcjh@umich.edu>
2018-04-11 15:24:24 -0400

    updates to UI

--- thread.py
+++ thread.py
@@ -10,9 +10,47 @@ import datetime
 import threading
 import queue
 
+import requests
+import json
+import datetime
+import codecs
+import sys
+
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
+
+CACHE_FNAME = 'dining_sites.json'
+try:
+    cache_file = open(CACHE_FNAME, 'r')
+    cache_contents = cache_file.read()
+    CACHE_DICTION = json.loads(cache_contents)
+    cache_file.close()
+
+except:
+    CACHE_DICTION = {}
+
+def get_unique_key(url):
+    return url
+
+def make_request_using_cache(url):
+    unique_ident = get_unique_key(url)
+
+    if unique_ident in CACHE_DICTION:
+        #print("Getting cached data...")
+        return CACHE_DICTION[unique_ident]
+
+    else:
+        #print("Making a request for new data...")
+        resp = requests.get(url)
+        CACHE_DICTION[unique_ident] = resp.text
+        dumped_json_cache = json.dumps(CACHE_DICTION)
+        fw = open(CACHE_FNAME,"w")
+        fw.write(dumped_json_cache)
+        fw.close()
+        return CACHE_DICTION[unique_ident]
+
 def getUrl(q, url):
     #print('getUrl('+url+') called from a thread.')
-    q.put(BeautifulSoup(urllib.request.urlopen(url), "html.parser"))
+    q.put(BeautifulSoup(make_request_using_cache(url), "html.parser"))
 
 
 def ribscraper(food_,start,numdays):
@@ -52,15 +90,12 @@ def ribscraper(food_,start,numdays):
             theurls.append(dining_day)
             #Query the website and return the html to the variable 'page'
 
-
-
     threadQueue = queue.Queue()
     for u in theurls:
         t = threading.Thread(target=getUrl, args = (threadQueue,u))
         t.daemon = True
         t.start()
-
-
+    
     for i in datelist:
         date = i.strftime("%Y-%m-%d")
         for hall in dining:
@@ -70,9 +105,9 @@ def ribscraper(food_,start,numdays):
             for row in food_table:
                 food = row.find(text=True)
                 if(food.find(food_) != -1):
-                    output += date + ": " + location[hall] + "<br>"
+                    output += "<div class='food' "+date + ": " + location[hall] + "<br>"
                     output += "<a href="+url+">" + row.find(text=True) + "</a>: Looks like we have this today!  <br>"
-                    output += "================================== <br><br>"
+                    output += "</div><br><br>"
 
     if (len(output) == (60 + len(food_))):
         output += "Oh noooo what you want isn't on the menu! :("
